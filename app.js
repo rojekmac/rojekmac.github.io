20e7e44e23862e2ee4f0755cee253a86515b27ad
rojekmac <rojek.mac@gmail.com>
2020-11-14 10:26:35 +0100

    button 2

--- app.js
+++ app.js
@@ -9,8 +9,22 @@ tweetForm.addEventListener('submit', function (e) {
     const newTweet = document.createElement('li');
     const bTag = document.createElement('b');
     const newButton = document.createElement("BUTTON");
+    const newButton2 = document.createElement("BUTTON");
 
     newButton.innerHTML = "Done!";  
+    newButton2.innerHTML = "Un-do!";
+
+    newButton2.addEventListener("click", myFunction2);
+
+function myFunction2() {
+    // newTweet.remove();
+    // newButton.remove();
+   
+    newTweet.style.textDecoration.toggle
+    newTweet.style.textDecorationColor.toggle
+    newTweet.style.color.toggle
+
+}
 
     newButton.addEventListener("click", myFunction);
 
